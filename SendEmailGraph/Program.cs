11dbd7bc400345d7fd993b73d36c2b5a992bcec1
Wagner Jaco <wagner.jaco@toolsort.com.br>
2024-11-12 09:16:27 -0300

    ok

--- SendEmailGraph/Program.cs
+++ SendEmailGraph/Program.cs
@@ -7,12 +7,7 @@ namespace SendEmailGraphGraph
 {
     public class Program
     {
-        //private static readonly string clientId = "4d592024-82e1-4eeb-b067-753fbcda8577";
-        //private static readonly string clientSecret = "pnC8Q~sAOx4D7O3fOm-aln6UO0dAXndc9gx6QdAz";
-        //private static readonly string tenantId = "3215374b-193f-44ca-aa45-6b25df64043e";
-        //private static readonly string graphApiUrl = "https://graph.microsoft.com/v1.0";
-        //private static readonly string user = "wagner.jaco@toolsort.com.br";
-        //private SmtpEmail smtpEmail;
+
         public static Profile profile;
         public static SendEmail _SendEmail;
         static async Task Main(string[] args)
